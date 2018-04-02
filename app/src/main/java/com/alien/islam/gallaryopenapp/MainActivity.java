633d72d4ed82347f3539308249a2ee8b407a440b
alien <alien@owl.com>
2018-04-02 16:38:41 +0200

    first commit

---
+++ app/src/main/java/com/alien/islam/gallaryopenapp/MainActivity.java
@@ -0,0 +1,57 @@
+package com.alien.islam.gallaryopenapp;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    final static int KEY = 777 ;
+    ImageView imageView ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        imageView = (ImageView) findViewById(R.id.imageView);
+
+
+    }
+
+
+    public void openGallery (View view){
+
+        Intent  intent = new Intent();
+        intent.setAction(intent.ACTION_GET_CONTENT);
+        intent.setType("image/*");
+        startActivityForResult(Intent.createChooser(intent,"CHOOSE YOUR FAVOURITE APP : "), KEY);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == KEY && resultCode == RESULT_OK && data != null && data.getData() != null) {
+
+            Uri uri = data.getData();
+
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                imageView.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+}
